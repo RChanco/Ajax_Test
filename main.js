80023a6df713cec9b5115d266434e0ba581d738d
RChanco <orabetic@hotmail.com>
2018-11-05 21:37:43 +0000

    Fix bug with commas rendering at the top of the table

--- main.js
+++ main.js
@@ -40,7 +40,12 @@ function writeToDocument(url){
     var el = document.getElementById("data");
 
     getData(url, function(data){
-        
+        var pagination = "";
+
+        if (data.next || data.previous) {
+            pagination = generatePaginationButtons(data.next, data.previous);
+        }
+
         data = data.results;
         var tableHeaders = getTableHeaders(data[0]);
         
@@ -55,6 +60,6 @@ function writeToDocument(url){
             tableRows.push(`<tr>${dataRow}</tr>`);
         });
 
-        el.innerHTML = `<table>${tableHeaders}${tableRows}</table>`;
+        el.innerHTML = `<table>${tableHeaders}${tableRows}</table>${pagination}`.replace(/,/g, "");
     });
 }
